9f45647c3bd51e3d93c67e1204ef454c39332810
namycodes <namycodes@yahoo.com>
2023-09-07 13:56:00 -0700

    Done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char myText[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", myText);
+	return (0);
+}
