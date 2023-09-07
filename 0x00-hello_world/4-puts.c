7ef57a653f8c803f0d3fc4acb31ce42a76bb9c6b
namycodes <namycodes@yahoo.com>
2023-09-07 13:33:17 -0700

    Done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main  - entry point
+ *
+ * Retun: always (sucess)
+ * This program uses the puts key world to output words to the console
+ */
 int main(void)
 {
-	char myString[] = "Programming is like building a multilingual puzzle";
+	char myString[] = "Programming is like building a multilingual puzzle\n";
 	puts(myString);
-	return 0;
+	return (0);
 }
