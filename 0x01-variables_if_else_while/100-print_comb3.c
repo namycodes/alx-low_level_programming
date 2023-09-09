108f708b8c945f5cf56399702eff362976d9423c
namycodes <namycodes@yahoo.com>
2023-09-08 19:35:07 -0700

    Done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,8 +3,6 @@
 /**
  * main - Entry point
  *
- * Description: Prints all possible different combinations of two digits from 0 to 89.
- *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -13,7 +11,7 @@ int main(void)
 
 	for (i = 0; i <= 8; i++)
 	{
-		for (j = i; j <= 9; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
