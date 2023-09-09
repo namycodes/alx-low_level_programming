9977682e1f60294d0a8bfe774a3a519ab7838c0a
namycodes <namycodes@yahoo.com>
2023-09-08 19:24:24 -0700

    Done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; j <= 9; j++)
+	for (i = 0; j <= 90; j++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j <= 90; j++)
 		{
 			putchar(i + '0');
 			putchar(',');
