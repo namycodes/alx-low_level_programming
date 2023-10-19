12cfa49d4713ddbd9e94819fee5bbf899d032196
namycodes <namycodes@yahoo.com>
2023-10-19 10:13:30 -0700

    Done

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * cmain - function executed before main
+ * Return: no return.
+ */
+
+void __attributes__ ((constructors)) cmain()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
