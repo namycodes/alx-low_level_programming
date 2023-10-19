469ab6867b6dad50caf27fb33d5fd0b58028192f
namycodes <namycodes@yahoo.com>
2023-10-19 09:26:07 -0700

    Done

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - outputs number of elements in a list
+ * @h: linked list
+ * Return: numbber of ele in a list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t number_of_elements;
+
+	number_of_elements = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		number_of_elements;
+	}
+	return (number_of_elements);
+}
