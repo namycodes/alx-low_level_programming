a9667dff498100953a5c1885c14a72da786b75a3
namycodes <namycodes@yahoo.com>
2023-10-19 09:46:57 -0700

    DOne

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,25 +5,19 @@
  * @str: string storing in list.
  * Return: address of the head.
  */
-
 list_t *add_node(list_t **head, const char *str)
 {
-        list_t *new_val;
-        size_t new_character;
+	list_t *new_val;
+	size_t new_character;
 
-        new_val = malloc(sizeof(list_t));
-        if (new_val == NULL)
-                return (NULL);
-
-        new_val->str = strdup(str);
-
-        for (new_character = 0; str[new_character]; new_character++)
-                ;
-
-        new_val->len = new_character;
-        new_val->next = *head;
-        *head = new_val;
-
-        return (*head);
+	new_val = malloc(sizeof(list_t));
+	if (new_val == NULL)
+		return (NULL);
+	new_val->str = strdup(str);
+	for (new_character = 0; str[new_character]; new_character++)
+		;
+	new_val->len = new_character;
+	new_val->next = *head;
+	*head = new_val;
+	return (*head);
 }
-
